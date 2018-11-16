922b8e373953a60bd22cfe2a773b260704714afb
aleksiz <45092547+aleksiz@users.noreply.github.com>
2018-11-16 12:48:21 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+ <title>Каталоги какие то</title>
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+ <link rel="stylesheet" href="style.css">
+</head>
+ <body>
+ 
+<div class="container">
+ <div class="row">
+	<div class="col-12">
+		<span class='TopText'>Журнал поручений</span>
+	</div>
+ </div>
+ <div class="row">
+	<div class="col-3">
+		
+	
+	</div>
+	<div class="col-6">
+	
+		<form class='index_form'>
+		  <div class="form-group">
+			<label for="exampleInputEmail1" style='margin-left:5px;'>Логин</label>
+			<input type="text" class="form-control" id="exampleInputEmail1" placeholder="Введите логин">
+			<small id="emailHelp" class="form-text text-muted" style='margin:0 auto; display:table;'>Введите логин, который вам выдал администратор Татьяна </small>
+		  </div>
+		  <div class="form-group">
+			<label for="exampleInputPassword1" style='margin-left:5px;'>Пароль</label>
+			<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Пароль">
+		  </div>
+		  <div class="form-group form-check">
+			<input type="checkbox" class="form-check-input" id="exampleCheck1">
+			<label class="form-check-label" for="exampleCheck1">Запомнить меня</label>
+		  </div>
+		  <button type="submit" class="btn btn-primary">Войти</button>
+		</form>
+		
+		<?php
+
+		?>
+		
+	</div>	
+	<div class="col-3">
+	
+	
+	</div>
+</div>
+ <div class="row">
+	<div class="col-12">
+		<span class='BottomText'>
+			Журнал поручений. Группа 147б. СГУ 2018	
+		</span>
+	</div>
+ </div>
+ </div>
+ 
+
+
+
+
+ </body>
+</html>
