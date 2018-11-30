2bcc063ee52cb18a1caee518f28258499f22a589
TNastya <45092711+TNastya@users.noreply.github.com>
2018-11-30 14:15:17 +0300

    Add files via upload

---
+++ adminka.php
@@ -0,0 +1,19 @@
+<form action="save_user.php" method = "post">
+	<center>
+	<table>
+		<tr>
+			<td colspan="2" align="center"><h3>Форма заполнения данных</h3></td>
+		</tr>	
+		<tr>
+			<td>Логин</td><td><input type = "text" name = "login"></td>
+		</tr>
+		<tr>
+			<td>Пароль</td><td><input type = "text"  name = "password"></td>
+		</tr>
+	</table>
+
+	<input type = "submit" name = "Soz" value = "Создать">
+	<input type = "submit" name = "Udl" value = "Удалить">
+	</center>
+
+</form>
