a49fe58ce91f1e0d7900f590da0144eae699372b
Killclowns <45092560+Killclowns@users.noreply.github.com>
2018-11-30 12:50:50 +0300

    Add files via upload

--- db.php
+++ db.php
@@ -2,16 +2,25 @@
 	$my_db = "journal";
 	$my_user = "root";
 	$my_pass = "";
-
-	$link = mysqli_connect ('localhost', $my_user, $my_pass, $my_db)
-
+	$link = mysqli_connect ('localhost', $my_user, $my_pass)
 	or die ('Не получилось');
-
-	mysqli_query ($link, "SET NAMES 'utf8");
-
-	$query = "select * from user";
-	$res - mysqli_query ($link, $query);
-
-	while ($row = mysqli_fetch_array ($res));
-		echo $row ['name'].$row ['phone']."<br>";
+	
+	$db_list = mysqli_query($link, "SHOW DATABASES");
+	
+	if (in_array ($my_db, mysqli_fetch_array($db_list)))
+	{
+		$link = mysqli_connect ('localhost', $my_user, $my_pass, $my_db);
+		mysqli_query ($link, "SET NAMES 'utf8'");
+	}
+	else
+	{
+		$create_db = 'CREATE DATABASE journal';
+		if( mysqli_query($link, $create_db) ){
+			echo "База my_db успешно создана\n";
+		} 
+		else 
+		{
+			//echo 'Ошибка при создании базы данных: ' . mysqli_error($link) . "\n";
+		}
+	}
 ?>
