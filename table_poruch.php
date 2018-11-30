7554f21775bcf3a5f259d584362d6e346225b7fe
IIEHOIIJIACT <45482024+IIEHOIIJIACT@users.noreply.github.com>
2018-11-30 14:18:09 +0300

    Update table_poruch.php

--- table_poruch.php
+++ table_poruch.php
@@ -12,13 +12,19 @@
 	
 	$query = "select opisanie, data_poruch, (select fio from task, user where user.idu=task.poruchitel), 
 			(select fio from task, user where user.idu=task.ispolnitel), srok, data_ispol, status from task";
-	$res - mysqli_query ($link, $query);
-	
-	//while ($row = mysqli_fetch_array ($res));
 
-
-echo "<div>Исполнено:  $row [fio]</div>";
+	for ($res - mysqli_query ($link, $query); $row = mysqli_fetch_array ($res);){
+		
+		
+	}
 	
+	while ($row = mysqli_fetch_array ($res));	
 	//echo Исполнено: $row ['fio'].
-	Исполнено: $row ['phone']."<br>";
+	
+	<table>
+ 		<tr>
+  		  <td>echo Исполнено: $row ['fio']</td>
+		  <td>echo Исполнено: $row ['fio']</td>
+		</tr>
+	</table>
 ?>
