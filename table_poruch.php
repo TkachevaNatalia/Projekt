919116e2245630e70b1544df83dc7f903368fd60
IIEHOIIJIACT <45482024+IIEHOIIJIACT@users.noreply.github.com>
2018-11-30 14:58:59 +0300

    Update table_poruch.php

--- table_poruch.php
+++ table_poruch.php
@@ -1,7 +1,7 @@
 <?php
 
-	$query = "select opisanie, data_poruch, (select FIO from task, user where user.idu=task.poruchitel), 
-			(select FIO from task, user where user.idu=task.ispolnitel), srok, data_ispol, status from task";
+	$query = "select opisanie, data_poruch, (select FIO from task, user where user.idu=task.poruchitel limit 1) as por, 
+			(select FIO from task, user where user.idu=task.ispolnitel limit 1) as isp, srok, data_ispol, status from task";
 
 	<table>
  		<tr>
@@ -17,12 +17,12 @@
 	while ($row = mysqli_fetch_array ($res)){
 		<tr>
   			<td>echo $row ['opisanie']</td>
-			<td>echo $row ['</td>
-			<td>Поручение</td>
-			<td>Поручение</td>
-			<td>Поручение</td>
-			<td>Поручение</td>
-			<td>Поручение</td>
+			<td>echo $row ['data_poruch']</td>
+			<td>echo $row ['por']</td>
+			<td>echo $row ['isp']</td>
+			<td>echo $row ['srok']</td>
+			<td>echo $row ['data_ispol']</td>
+			<td>echo $row ['status']</td>
 		</tr>
 	}
 	
